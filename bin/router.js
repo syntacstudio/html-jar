0e97c3575dfdedf6e6646fad83928910c7a9228f
tofik_hidayat <tofik@syntac.co.id>
2019-06-17 08:32:34 +0700

    Add route  parent

---
+++ bin/router.js
@@ -0,0 +1,120 @@
+"use strict"
+/**
+** Creating expert router
+**/
+import { App ,  Route  } from "./skeleton";
+import { Render  } from "./render";
+
+use("app/kernel");
+global["Routes"] =  {};
+let _route = {};
+/**
+** registering components
+**/
+
+/**
+** Global creator
+**/
+
+// Method get 
+async function Get(route,controller,param=null) {
+	// creatign global name
+	try {
+		global["Routes"][param["name"]] =  route;
+	} catch(e) {}
+	Route.get(route,csrfProtection, async function(req,res,next){
+		let _res = {};
+		let _next  = function() {
+			return next();
+		}
+		for(var item in res) {
+			let iname  = item;
+			_res[item] =  function(prop) {
+				return res[iname](prop);
+			} 
+		}
+		async function _send() {
+			return new Promise(async function(resolve,reject) {
+				var render =   await Render(req,_res,_next,controller);
+				Promise.resolve(res.send(render))
+			})
+		}
+		if (param) {
+			if (param["middleware"]) {
+				try {
+					if (typeof param.middleware == 'object' ) {
+						for (var i = 0; i <  param.middleware.length; i++) {
+							return middleware[param.middleware[i]].run({req:req,res:_res,next:_next}) === true ? _send() : false;		
+						}
+					}
+					else {
+						return middleware[param.middleware].run({req:req,res:_res,next:_next}) === true ? _send() : false;
+					}
+				} catch (e) {
+					throw e;
+				}
+			}
+		}
+		return await _send();
+	})
+}
+// Method post
+async function Post(route,controller,param=null) {
+	// creatign global name
+	try {
+		global["Routes"][param["name"]] =  route;
+	} catch(e) {}
+	Route.post(route, parseForm, csrfProtection,async function(req,res,next){
+		let _res = {};
+		let _next  = function() {
+			return next();
+		}
+		for(var item in res) {
+			let iname  = item;
+			_res[item] =  function(prop) {
+				return res[iname](prop);
+			} 
+		}
+		async function _send() {
+			return new Promise(async function(resolve,reject) {
+				var render =   await Render(req,_res,_next,controller);
+				Promise.resolve(res.send(render))
+			})
+		}
+		if (param) {
+			if (param["middleware"]) {
+				try {
+					if (typeof param.middleware == 'object' ) {
+						for (var i = 0; i <  param.middleware.length; i++) {
+							return middleware[param.middleware[i]].run({req:req,res:_res,next:_next}) === true ? _send() : false;		
+						}
+					}
+					else {
+						return middleware[param.middleware].run({req:req,res:_res,next:_next}) === true ? _send() : false;
+					}
+				} catch (e) {
+					throw e;
+				}
+			}
+		}
+		return await _send();
+	})
+}
+/*
+
+async function Group(route,func) {
+	_route.Get =  function(route,controller,param=null) {
+		return Get(route,controller,param);
+	}
+	return Route.group("/bambang",function(_route) {
+		console.log(_route)
+	})
+
+}    
+
+*/
+module.exports  = {
+	Get ,
+	Post ,
+	//Group
+};
