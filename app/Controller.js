df0c108924adc67b481bc882a8eaca62df3ac846
tofik_hidayat <tofik@syntac.co.id>
2019-06-17 08:28:09 +0700

    Add controller base reflector

---
+++ app/Controller.js
@@ -0,0 +1,13 @@
+"use strict"
+/**
+** This file is for creatign global function 
+** But only can access in controller
+** @param controller
+**/
+class Controller {
+	constructor() {
+		
+	}
+}
+
+module.exports =  Controller
