f14d93312183ce4abb169385490568e0b6b8ba1b
tofik_hidayat <tofik@syntac.co.id>
2019-06-17 08:28:20 +0700

    Add middleware autoloadder

---
+++ app/kernel.js
@@ -0,0 +1,38 @@
+/**
+** Making middleware
+** @param middleware  contructor
+**/
+const middlewares  =  {
+	Auth:'app.middleware.auth'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Migrate to global
+global["middleware"] = {}
+for(var i in middlewares ) {
+	eval(` var ${i} =  ${use(middlewares[i])}`);
+	global.middleware[i] =  eval(`new ${i}()`);
+}
