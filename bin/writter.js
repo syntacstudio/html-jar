28bac0ab08276e130b0a2ae853eb5a5a27ae461a
tofik_hidayat <tofik@syntac.co.id>
2019-06-18 10:53:49 +0700

    Fix writter event

--- bin/writter.js
+++ bin/writter.js
@@ -3,7 +3,8 @@ const fs = require("fs");
 const chokidar = require('chokidar');
 const path =  require("path");
 const recursiveReadSync = require('recursive-readdir-sync');
-const watchTarget = [base("/resources/views")];
+const watchTarget = [base("/resources/views/")];
+const fse = require("fs-extra");
 const protect  =  require(base("/helpers/protector.json"));
 /**
 ** Application write to storage
@@ -21,20 +22,63 @@ const protector = (file)=>{
 	return stat == 0 ? file  : false; 
 }
 // compiling all data
-const compileAll = ()=>{
-
+const compileAll = async ()=>{
+	let indexed =  await recursiveReadSync(watchTarget[0]);
+	for(var i in indexed) {
+		let protect  =  protector(indexed[i].toString());
+		if (protect) {
+			await doCompile(protect);
+		}
+	}
 }
-// watcher
+//  to write file 
+const doCompile = async(file)=>{
+	file  =  file.replace(".edge","");
+	return await write(await view(file),file);
+}
+// write to file
+const write  =  async(content,name)=>{
+	name  = base("/public"+name+".html");
+	return await fse.outputFile(name , content, (err) => {
+ 		if (err)  console.log(`[Status : failed to write] [Message : ${err}]`);
+ 		console.log(`[Write : succcess] [Directory : ${name}]`)
+ 	})
+}
+// remove file
+const doRemove =  (name)=>{
+	name  = base("/public"+name.replace(".edge","")+".html");
+	fse.remove(name,(err)=>{
+		if (err)  console.log(`[Status : failed to delete] [Message : ${err}]`);
+ 		console.log(`[Delete : succcess] [Directory : ${name}]`)
+	})	
+	return;
+}
+// watcher 
+let readyStat = 0
 const watcher  = chokidar.watch(watchTarget)
 	  .on("ready",()=>{
-	  	 console.log("Watcher file allready to action");
+	  	 console.log(`[Watcher status : ready]`);
+	  	 readyStat =  1;
 	  })
-	  .on("add",(file)=> {
-	  	 console.log(protector(file))
+	  .on("add", async(file)=> {
+	  	 if (!file.includes(".edge")) return false;
+	  	 let protect  =  protector(file);
+	  	 if (readyStat == 0) {
+	  	 	return protect ? await doCompile(protect) : false;
+	  	 } else {
+	  	  	return protect ? await doCompile(protect) : await compileAll();
+	  	 }
 	  })
-	  .on("change",(file)=>{
-	  	 console.log(file);
+	  .on("change", async (file)=>{
+	  	 if (!file.includes(".edge")) return false;
+	  	 let protect  =  protector(file);
+	  	 return protect ? await doCompile(protect) : await compileAll();
 	  })
-	  .on("unlink",(file)=>{
-	  	console.log(file)
+	  .on("unlink", async (file)=>{
+	  	 if (!file.includes(".edge")) return false;
+	  	 let protect  =  protector(file);
+	  	 if (protect) {
+	  	 	 return await doRemove(protect);
+	  	 }
+	  	 return compileAll()
 	  })
