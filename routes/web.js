577e5c62dd5e34c9b12b5b8d6ba82056cb450fab
tofikhidayat <tofik@syntac.co.id>
2019-07-19 00:48:05 +0700

    router method

--- routes/web.js
+++ routes/web.js
@@ -1,8 +1,19 @@
 "use strict"
-const Route =  use("bin/router/web")
+const Route =  use("bin/router/web").routerMethod
 /**
-** Creating base route 
-**/ 
-Route.get("/",HomeController.index,{name:"home",middleware:"auth"});
+** Creating base route
+**/
 
-Route.get("/demo/:id/:key",HomeController.index,{name:"test"});
+Route.get('/','HomeController@index').name("test").middleware(['auth']).exec()
+
+/*
+Route.prefix('/home').groupMiddleware(['auth']).groupName("home.").group((Route)=>{
+
+	Route.get('/','HomeController@api')
+	Route.get('/hello','HomeController@index').name("test").middleware(['examination'])
+})
+
+
+
+
+*/
